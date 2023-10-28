7d2dd1b9af4ba7150e41bd088c4378c33295806d
sireeshachowdary32 <sireeshachowdary32@gmail.com>
2023-10-27 21:01:40 -0500

    Added TeamlayoutActivity class and updated activity_team_layout layout file

---
+++ app/src/main/java/com/example/retailermanagementplatform/TeamLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.retailermanagementplatform;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeamLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_layout);
+    }
+}
